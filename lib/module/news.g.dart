ab2319eccd826b2083ca3efdcac4d40c3ca2c992
ydd-mac <1019132983@qq.com>
2019-01-18 14:48:45 +0800

    添加解析

---
+++ lib/module/news.g.dart
@@ -0,0 +1,84 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'news.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+News _$NewsFromJson(Map<String, dynamic> json) {
+  return News(
+      json['curPage'] as int,
+      (json['datas'] as List)
+          ?.map((e) =>
+              e == null ? null : Datas.fromJson(e as Map<String, dynamic>))
+          ?.toList(),
+      json['offset'] as int,
+      json['over'] as bool,
+      json['pageCount'] as int,
+      json['size'] as int,
+      json['total'] as int);
+}
+
+Map<String, dynamic> _$NewsToJson(News instance) => <String, dynamic>{
+      'curPage': instance.curPage,
+      'datas': instance.datas,
+      'offset': instance.offset,
+      'over': instance.over,
+      'pageCount': instance.pageCount,
+      'size': instance.size,
+      'total': instance.total
+    };
+
+Datas _$DatasFromJson(Map<String, dynamic> json) {
+  return Datas(
+      json['apkLink'] as String,
+      json['author'] as String,
+      json['chapterId'] as int,
+      json['chapterName'] as String,
+      json['collect'] as bool,
+      json['courseId'] as int,
+      json['desc'] as String,
+      json['envelopePic'] as String,
+      json['fresh'] as bool,
+      json['id'] as int,
+      json['link'] as String,
+      json['niceDate'] as String,
+      json['origin'] as String,
+      json['projectLink'] as String,
+      json['publishTime'] as int,
+      json['superChapterId'] as int,
+      json['superChapterName'] as String,
+      json['tags'] as List,
+      json['title'] as String,
+      json['type'] as int,
+      json['userId'] as int,
+      json['visible'] as int,
+      json['zan'] as int);
+}
+
+Map<String, dynamic> _$DatasToJson(Datas instance) => <String, dynamic>{
+      'apkLink': instance.apkLink,
+      'author': instance.author,
+      'chapterId': instance.chapterId,
+      'chapterName': instance.chapterName,
+      'collect': instance.collect,
+      'courseId': instance.courseId,
+      'desc': instance.desc,
+      'envelopePic': instance.envelopePic,
+      'fresh': instance.fresh,
+      'id': instance.id,
+      'link': instance.link,
+      'niceDate': instance.niceDate,
+      'origin': instance.origin,
+      'projectLink': instance.projectLink,
+      'publishTime': instance.publishTime,
+      'superChapterId': instance.superChapterId,
+      'superChapterName': instance.superChapterName,
+      'tags': instance.tags,
+      'title': instance.title,
+      'type': instance.type,
+      'userId': instance.userId,
+      'visible': instance.visible,
+      'zan': instance.zan
+    };
---
+++ lib/module/user_detail.g.dart
@@ -0,0 +1,33 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'user_detail.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+UserDetail _$UserDetailFromJson(Map<String, dynamic> json) {
+  return UserDetail(
+      json['chapterTops'] as List,
+      json['collectIds'] as List,
+      json['email'] as String,
+      json['icon'] as String,
+      json['id'] as int,
+      json['password'] as String,
+      json['token'] as String,
+      json['type'] as int,
+      json['username'] as String);
+}
+
+Map<String, dynamic> _$UserDetailToJson(UserDetail instance) =>
+    <String, dynamic>{
+      'chapterTops': instance.chapterTops,
+      'collectIds': instance.collectIds,
+      'email': instance.email,
+      'icon': instance.icon,
+      'id': instance.id,
+      'password': instance.password,
+      'token': instance.token,
+      'type': instance.type,
+      'username': instance.username
+    };
